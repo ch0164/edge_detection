e782eb5b65b16eaf97e6c2c6cef5b6332bf3e67a
Christian Hall <ch0164@uah.edu>
2022-02-12 01:07:30 -0600

    Reverted edge_detection.cpp; cleaning up memory leak.

--- source/main.cpp
+++ source/main.cpp
@@ -9,6 +9,8 @@ int main(void) {
 	int **output_image = new int*[IMAGE_HEIGHT - 2];
 	for (int i = 0; i < IMAGE_HEIGHT; i++) {
 		input_image[i] = new int[IMAGE_WIDTH];
+	}
+	for (int i = 0; i < IMAGE_HEIGHT - 2; i++) {
 		output_image[i] = new int[IMAGE_WIDTH - 2];
 	}
 	
@@ -50,5 +52,75 @@ int main(void) {
 		write_pgm(image_label + "_roberts", output_image);
 	}
 
+    /* Clean up allocated memory. */
+    for (int i = 0; i < IMAGE_HEIGHT; i++) {
+		delete[] input_image[i];
+	}
+	for (int i = 0; i < IMAGE_HEIGHT - 2; i++) {
+		delete[] output_image[i];
+	}
+	delete[] input_image;
+	delete[] output_image;
+
     return 0;
+}
+
+int sobel(const bool vertical, const int i, const int j, int **I) {
+	if (vertical) {
+		return I[i - 1][j - 1] * SOBEL_V[0][0]
+			 + I[i - 1][j] * SOBEL_V[0][1]
+			 + I[i - 1][j + 1] * SOBEL_V[0][2]
+			 + I[i][j - 1] * SOBEL_V[1][0]
+			 + I[i][j] * SOBEL_V[1][1]
+			 + I[i][j + 1] * SOBEL_V[1][2]
+			 + I[i + 1][j - 1] * SOBEL_V[2][0]
+			 + I[i + 1][j] * SOBEL_V[2][1]
+			 + I[i + 1][j + 1] * SOBEL_V[2][2];
+	}
+	else {
+		return I[i - 1][j - 1] * SOBEL_H[0][0]
+			 + I[i - 1][j] * SOBEL_H[0][1]
+			 + I[i - 1][j + 1] * SOBEL_H[0][2]
+			 + I[i][j - 1] * SOBEL_H[1][0]
+			 + I[i][j] * SOBEL_H[1][1]
+			 + I[i][j + 1] * SOBEL_H[1][2]
+			 + I[i + 1][j - 1] * SOBEL_H[2][0]
+			 + I[i + 1][j] * SOBEL_H[2][1]
+			 + I[i + 1][j + 1] * SOBEL_H[2][2];
+	}
+}
+
+int roberts(const bool vertical, const int i, const int j, int **I) {
+	if (vertical) {
+		return I[i][j] * ROBERTS_V[0][0]
+			 + I[i][j + 1] * ROBERTS_V[0][1]
+			 + I[i + 1][j] * ROBERTS_V[1][0]
+			 + I[i + 1][j + 1] * ROBERTS_V[1][1];
+	}
+	else {
+		return I[i][j] * ROBERTS_H[0][0]
+			 + I[i][j + 1] * ROBERTS_H[0][1]
+			 + I[i + 1][j] * ROBERTS_H[1][0]
+			 + I[i + 1][j + 1] * ROBERTS_H[1][1];
+	}
+}
+
+void write_pgm(const std::string& label, int **W) {
+	std::ofstream pgm(OUTPUT_DIR + label + ".pgm");
+	
+    if (!pgm) {
+		std::cerr << "Error occurred opening " << label << std::endl;
+		return;
+	}
+
+	pgm << "P2" << std::endl;  // Header
+	pgm << IMAGE_WIDTH - 2 << " " << IMAGE_HEIGHT - 2 << std::endl;  // Dimensions
+	pgm << "255" << std::endl;  // Maximum Value
+
+	for (int i = 0; i < IMAGE_HEIGHT - 2; i++) {
+		for (int j = 0; j < IMAGE_WIDTH - 2; j++) {
+			pgm << W[i][j] << " ";
+		}
+		pgm << std::endl;
+	}
 }
