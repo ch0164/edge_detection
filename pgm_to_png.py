a9e53b38a8422075b9f01cfd6d5efaab1ddb60af
Christian <christianwhall41@gmail.com>
2022-03-27 07:37:07 -0500

    Results

--- pgm_to_png.py
+++ pgm_to_png.py
@@ -1,7 +1,7 @@
 import os
 
 PWD = os.getcwd()
-LABELS = ["cameraman", "lena", "mandril", "pirate"]
+LABELS = ["cameraman", "lena", "mandril", "pirate", "peppers", "house"]
 OUTPUT_DIRECTORIES = [f"{PWD}/output/{label}/histogram/" for label in LABELS]
 CAMERAMAN_DIR = f"{PWD}\\output\\cameraman\\histogram\\"
 
@@ -9,10 +9,11 @@ def main():
     #for dir in OUTPUT_DIRECTORIES:
     print(PWD)
     print(CAMERAMAN_DIR)
-    for file in os.listdir(CAMERAMAN_DIR):
-        if not "png" in file and not "csv" in file:
-            command = f"convert {CAMERAMAN_DIR}{file} {CAMERAMAN_DIR}png/{file.split('.pgm')[0] + '.png'}"
-            os.system(command)
+    for dir in OUTPUT_DIRECTORIES:
+        for file in os.listdir(dir):
+            if not "png" in file and not "csv" in file:
+                command = f"convert {dir}{file} {dir}png/{file.split('.pgm')[0] + '.png'}"
+                os.system(command)
 
 if __name__ == "__main__":
     main()
